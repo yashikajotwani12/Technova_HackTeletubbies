fd8ad858bbe4c4001f1445719362a35c9747a9d9
Yashika <yashikajotwani12@gmail.com>
2021-08-28 22:14:50 +0530

    done

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic import ListView
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'Home/home.html')
+
+class home(ListView):
+    template_name='Home/home.html'
