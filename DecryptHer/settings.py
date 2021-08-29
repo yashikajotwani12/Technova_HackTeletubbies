5dde2c3a58bec8dd67fe038779eea082a958ad56
Yashika <yashikajotwani12@gmail.com>
2021-08-29 14:00:42 +0530

    done

--- DecryptHer/settings.py
+++ DecryptHer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s$#+4wznhl1cpp)ji%eh1ob$$sr9eyxi(22_$m00@f_w76k9w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn DecryptHer.wsgi --log-file -
